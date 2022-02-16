3aa29841aefaa0cc0ef66d2d9eb50b0d8c88ffca
Jody-septiawan <jody.septiawan5@gmail.com>
2022-02-16 19:29:24 +0900

    fix socket cors

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ const io = new Server(server, {
   cors: {
     origin: process.env.CLIENT_URL || 'http://localhost:3000', // we must define cors because our client and server have diffe
     methods: ['GET', 'POST'],
+    credentials: true,
   },
 });
 
